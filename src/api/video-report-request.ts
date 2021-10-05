6566451adfdb53a885676c9c0ba886e5181f7370
Luke Selden <luke.selden@vbrick.com>
2021-10-05 12:01:46 -0400

    include video-report request class

---
+++ src/api/video-report-request.ts
@@ -0,0 +1,140 @@
+import type { RevClient } from '../rev-client';
+import type { Rev, Video } from '../types';
+import { asValidDate } from '../utils';
+import { IPageResponse, PagedRequest } from '../utils/paged-request';
+
+
+const DEFAULT_INCREMENT: number = 30;
+const DEFAULT_SORT: Rev.SortDirection = 'asc';
+
+function addDays(date: Date, numDays: number)
+{
+    const d = new Date(date.getTime());
+    d.setDate(d.getDate() + numDays);
+    return d;
+}
+
+function parseOptions(options: Video.VideoReportOptions) {
+    let {
+        incrementDays = DEFAULT_INCREMENT,
+        sortDirection = DEFAULT_SORT,
+        videoIds,
+        startDate,
+        endDate,
+        ...otherOptions
+    } = options;
+
+    // clamp increment to 1 minute - 30 days range
+    incrementDays = Math.min(
+        Math.max(
+            1 / 24 / 60,
+            parseFloat(incrementDays as any) || DEFAULT_INCREMENT
+        ),
+        30
+    );
+
+    // API expects videoIds as a string
+    if (Array.isArray(videoIds)) {
+        videoIds = videoIds
+            .map(s => s.trim())
+            .filter(Boolean)
+            .join(',');
+    }
+
+    return {
+        incrementDays, sortDirection, videoIds,
+        ...parseDates(startDate, endDate),
+        ...otherOptions
+    };
+}
+
+function parseDates(startArg: string | Date | undefined, endArg: string | Date | undefined) {
+    const now = new Date();
+    let startDate = asValidDate(startArg);
+    let endDate = asValidDate(endArg);
+
+    // if no end date set then use now, or startDate + 30 days
+    if (!endDate) {
+        if (startDate) {
+            endDate = addDays(startDate, 30);
+            if (endDate.getTime() > now.getTime()) {
+                endDate = now;
+            }
+        } else {
+            endDate = now;
+        }
+    }
+    // if no start/beginning date then use end - 30 days
+    if (!startDate)
+    {
+        startDate = addDays(endDate, -30);
+    }
+
+    // make sure times aren't swapped
+    if (startDate.getTime() > endDate.getTime()) {
+        [startDate, endDate] = [endDate, startDate];
+    }
+    return { startDate, endDate };
+}
+
+export class VideoReportRequest extends PagedRequest<Video.VideoReportEntry> {
+    declare options: Required<ReturnType<typeof parseOptions>>;
+    private _rev: RevClient;
+    constructor(rev: RevClient, options: Video.VideoReportOptions = {}) {
+        super(parseOptions(options));
+
+        this._rev = rev;
+    }
+    protected async _requestPage() {
+        const { startDate, endDate } = this;
+        const {incrementDays, sortDirection, videoIds } = this.options;
+        const isAscending = sortDirection === 'asc';
+
+        let rangeStart = startDate;
+        let rangeEnd = endDate;
+        let done = false;
+
+        if (isAscending) {
+            rangeEnd = addDays(rangeStart, incrementDays);
+            //
+            if (rangeEnd >= endDate) {
+                done = true;
+                rangeEnd = endDate;
+            }
+        } else {
+            rangeStart = addDays(rangeEnd, -1 * incrementDays);
+            if (rangeStart <= startDate)
+            {
+                done = true;
+                rangeStart = startDate;
+            }
+        }
+
+        const query: Record<string, string> = {
+            after: rangeStart.toJSON(),
+            before: rangeEnd.toJSON()
+        };
+        if (videoIds) {
+            query.videoIds = videoIds;
+        }
+        const items: Video.VideoReportEntry[] = await this._rev.get("/api/v2/videos/report", query, { responseType: "json" });
+
+        // go to next date range
+        if (!done) {
+            if (isAscending) {
+                this.startDate = rangeEnd;
+            } else {
+                this.endDate = rangeStart;
+            }
+        }
+
+        return {
+            items,
+            done
+        };
+    }
+    get startDate() { return this.options.startDate; }
+    set startDate(value) { this.options.startDate = value; }
+    get endDate() { return this.options.endDate; }
+    set endDate(value) { this.options.endDate = value; }
+}
